8b084c5447fa7cebb92c6abd1f490c4feafd871f
Ricardo263 <72820083+Ricardo263@users.noreply.github.com>
2020-10-19 00:31:56 +0100

    Add files via upload

---
+++ 30864 Ricardo Andrade/array-filter.js
@@ -0,0 +1,13 @@
+function getShortMessages(messages) {
+
+  return messages.filter(function(obj) {
+    
+      return obj.message.length <= 50;
+    }).map(function(obj) { return obj.message;});
+}
+
+  module.exports = getShortMessages
+
+ 
+
+
