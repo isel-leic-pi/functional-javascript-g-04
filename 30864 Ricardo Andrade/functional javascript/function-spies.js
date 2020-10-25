e1179e3b25b39ff46f1e3bf36181bf72dae01e3d
Ricardo263 <72820083+Ricardo263@users.noreply.github.com>
2020-10-25 18:12:18 +0000

    Add files via upload

---
+++ 30864 Ricardo Andrade/functional javascript/function-spies.js
@@ -0,0 +1,32 @@
+function Spy(target, method) {
+	let spy = { count : 0 }
+	let oldFn = target[method]
+	target[method] = function (){
+		spy.count++
+		return oldFn.apply(this, arguments)
+	}
+	return spy
+}
+module.exports = Spy
+
+/*
+function Spy(target, method) {
+      var originalFunction = target[method]
+
+      // use an object so we can pass by reference, not value
+      // i.e. we can return result, but update count from this scope
+      var result = {
+        count: 0
+      }
+
+      // replace method with spy method
+      target[method] = function() {
+        result.count++ // track function was called
+        return originalFunction.apply(this, arguments) // invoke original function
+      }
+
+      return result
+    }
+
+    module.exports = Spy
+*/
